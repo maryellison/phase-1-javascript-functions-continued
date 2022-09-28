66d454e2a7a76460786d6ef8f97f5e2f37d213ce
Mary Ellison Barlow <maryellisonbarlow@gmail.com>
2022-09-28 18:48:26 -0400

    commit tree 7d7e1ab91bb63b46ae1886de7de997675e3f500e

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+  }
+  saturdayFun();
+  saturdayFun("bathe my dog");
+
+  function mondayWork(activity = "go to the office"){
+    return (`This Monday, I will ${activity}.`);
+  }
+  mondayWork();
+  mondayWork("work from home");
+
+  function wrapAdjective(flair = "*"){
+    const description = "special";
+    return function (description){
+        return (`You are ${flair}${description}${flair}!`);
+    };
+  }
+  wrapAdjective();
+  wrapAdjective()("a hard worker");
+  wrapAdjective("||")("a dedicated programmer");
+  
